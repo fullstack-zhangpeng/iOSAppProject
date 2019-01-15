0c8443e17a780c6ecc38281ec97aa2e4c2d66376
zhangpeng <zhangpeng01@zuoyebang.com>
2019-01-15 16:16:04 +0800

    delete done

--- iOSAppProject/ProjecFiles/HomeViewController.m
+++ iOSAppProject/ProjecFiles/HomeViewController.m
@@ -11,7 +11,7 @@
 #import "UIViewController+Category.h"
 #import "GCDWebServerDataResponse.h"
 #import "LoginManager.h"
-#import "TViewController.h"
+
 @interface HomeViewController () <UITextFieldDelegate>
 
 @end
@@ -22,26 +22,9 @@
     NSLog(@"函数名：%s，行号：%d", __FUNCTION__, __LINE__);
 }
 
-- (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
-    TViewController *vc = [[TViewController alloc] init];
-//    vc.hideLeftBarButtonItem = YES;
-    vc.hideHomeIndicator = YES;
-    [self.navigationController pushViewController:vc animated:YES];
-}
-
 - (void)viewDidLoad {
     [super viewDidLoad];
     
-    [self setupRightItem];
-}
-
-- (void)clickRightItem {
-    NSLog(@"函数名：%s，行号：%d", __FUNCTION__, __LINE__);
-}
-
-- (void)setupRightItem {
-    UIBarButtonItem *rightItem = [[UIBarButtonItem alloc] initWithTitle:@"设置" style:UIBarButtonItemStylePlain target:self action:@selector(clickRightItem)];
-    self.navigationItem.rightBarButtonItem = rightItem;
 }
 
 - (void)didReceiveMemoryWarning {
